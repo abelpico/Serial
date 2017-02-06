ed10eec31098f2b2ab1f35a88cdbedb4629054c3
abelpico <abel.pico94@gmail.com>
2017-02-06 20:59:53 +0100

    Math is fun

---
+++ MathIsFun1/MathIsFun1.ino
@@ -0,0 +1,57 @@
+/****************************************************************
+**                                                             **
+**                            TÍTOL:                           **
+**                         Math is fun1                        **
+**                                                             **
+**   NOM: Abel Picó                           Data:06/02/17    **
+****************************************************************/
+//************************** INCLUDE ****************************
+
+
+//************************* VARIABLES ***************************
+
+int a = 5;
+int b = 10;
+int c = 20;
+
+//*************************** SETUP *****************************
+
+void setup() {
+  
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  Serial.println("Here is some math: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+  Serial.print("c = ");
+  Serial.println(c);
+
+  Serial.print("a + b = ");       // add
+  Serial.println(a + b);
+
+  Serial.print("a * c = ");       // multiply
+  Serial.println(a * c);
+  
+  Serial.print("c / b = ");       // divide
+  Serial.println(c / b);
+
+  Serial.print("c % b = ");       // module
+  Serial.println(c % b);
+  
+  Serial.print("b - c = ");       // subtract
+  Serial.println(b - c);
+}
+
+
+//*************************** LOOP ******************************
+
+void loop() {
+  
+
+}
+
+
+//************************ FUNCIONS *****************************
